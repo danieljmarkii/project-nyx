b62616ee03db9e8bd0788c1a1b9ea852c88783f7
danieljmarkii <47282105+danieljmarkii@users.noreply.github.com>
2026-05-23 13:45:23 +0000

    Revert "Update _layout.tsx"
    This reverts commit ee1ada0c3c60510d641d64c86483ebb819d3d3c0.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,28 +7,8 @@ import { usePetStore } from '../store/petStore';
 import { initDb } from '../lib/db';
 import { useSync } from '../hooks/useSync';
 import { Toast } from '../components/ui/Toast';
-import * as Sentry from '@sentry/react-native';
 
-Sentry.init({
-  dsn: 'https://b5dcad0b8ee1274703b67be87b1b52c6@o4511425413054464.ingest.us.sentry.io/4511425567457280',
-
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
-
-  // Enable Logs
-  enableLogs: true,
-
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration()],
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
-});
-
-export default Sentry.wrap(function RootLayout() {
+export default function RootLayout() {
   const { setSession, setLoading } = useAuthStore();
   const { isOnboarded } = usePetStore();
 
@@ -75,4 +55,4 @@ export default Sentry.wrap(function RootLayout() {
       <Toast />
     </>
   );
-});
+}
